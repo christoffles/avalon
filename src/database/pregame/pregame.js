7102c42bf829bd750da29d798e7e16bd44661de7
Christoffles <www.chris@hotmail.ca>
2018-08-02 22:09:04 -0400

    no config values in repo - pass in as env variables later, negligible changes to poc

--- src/database/pregame/pregame.js
+++ src/database/pregame/pregame.js
@@ -1,18 +1,20 @@
 import { database } from '../config'
 
+// caller sets redux state as 'in pre-game' - afterwards, updateState will update
 export const createRoom = (userId, updateState) => {
+    const roomId = 0; // todo generate unique room ids
     // create the room
-    database.ref('games/' + userId).set({
+    database.ref('games/' + roomId).set({
         owner: userId,
         status: 'open',
         participants: [ userId ]
     });
 
     // attach a listener
-    // upateState is a function that dispatches an action to keep redux state consistent with realtime db
-    database.ref('games/' + userId).on('value', (snapshot) => {
-        updateState(snapshot)
-
+    // updateState is a function that dispatches an action to keep redux state consistent with realtime db
+    database.ref('games/' + roomId).on('value', (snapshot) => {
+        updateState(snapshot);
+        console.log(snapshot);
     });
 };
 
