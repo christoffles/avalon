65e0657861b62de5af1904756239af8707ca864f
Chris <chrisl@integrate.ai>
2018-08-09 11:20:44 -0400

    Add redux forms, user id form poc

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import { store } from './src/store'
 import { HomeContainer } from "./src/containers/home";
+import { SignupContainer } from "./src/containers/signup";
+
+global.navigator = { userAgent: 'all' };
 
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <HomeContainer />
-        </View>
+        <MuiThemeProvider>
+          <View style={styles.container}>
+            <SignupContainer />
+          </View>
+        </MuiThemeProvider>
       </Provider>
     );
   }
